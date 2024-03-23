6893ebd013c8b811d825da147d7df211caec7daf
jeevjacobgeorge <jeev@J4Hp>
2024-03-23 22:26:16 +0530

    Added admin home

--- print3/users/views.py
+++ print3/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
-from studapi.models import Lab,File,Student
+from studapi.models import Lab,File,Student,Experiment
 from django.http import JsonResponse
 import json
 
@@ -28,26 +28,26 @@ def home(request):
     elif request.method == 'POST':
         batch = request.POST.get('batch')
         lab = request.POST.get('lab')
-        experiments = File.objects.filter(lab_id__lab_name=lab)
+        experiments = Experiment.objects.filter(lab_id__lab_name=lab)
+        exp_name_list =  sorted(experiments.values_list('exp_name', flat=True)) 
         data_dict = {}
         
         students = Student.objects.filter(batch=batch)
-        
+        roll_list = []
+        uploaded = []
+        urls = []
         for student in students:
-            data_dict[student.roll_no] = {}
-            
-            for exp in experiments:
-                data_dict[student.roll_no][exp.exp_id] = {}
-                
+            roll_list.append(student.roll_no)
+            for exp in experiments:                
                 try:
-                    file_instance = File.objects.get(std_id=student, lab_id=exp.lab_id, exp_id=exp.exp_id)
-                    data_dict[student.roll_no][exp.exp_id]['uploaded'] = True
-                    data_dict[student.roll_no][exp.exp_id]['url'] = file_instance.file.url
+                    file_instance = File.objects.get(std_id=student, lab_id=exp.lab_id, exp_id=exp.id)
+                    uploaded.append(True) 
+                    urls.append(file_instance.file.url)
                 except File.DoesNotExist:
-                    data_dict[student.roll_no][exp.exp_id]['uploaded'] = False
-                    data_dict[student.roll_no][exp.exp_id]['url'] = None
+                    uploaded.append(False)
+                    urls.append(None)
                             
-        return render(request, 'users/home.html', {'experiments': data_dict, 'batches': batches})
+        return render(request, 'users/home.html', {'roll_list': roll_list, 'batches': batches,'exp_names_list':exp_name_list,'uploaded':uploaded,'urls':urls})
     
     else:
         return JsonResponse({'error': 'Invalid request'}, status=400)
