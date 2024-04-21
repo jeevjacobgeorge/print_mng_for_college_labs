83d10e762dd8df88d5b61b6a6831e0d71b08fd49
jeevjacobgeorge <jeev@J4Hp>
2024-04-21 23:25:58 +0530

    Fixed upload in student react

--- ProjectForm/src/Components/Dashboard.jsx
+++ ProjectForm/src/Components/Dashboard.jsx
@@ -1,96 +1,124 @@
-import React from 'react'
-import  { useState, useEffect } from 'react';
-import '../assets/dashboard.css'
-const Dashboard = () => {
+import React, { useState, useEffect } from 'react';
+import '../assets/dashboard.css';
+import { Link } from 'react-router-dom';
+import profilepic from '../assets/profilepic.svg';
+import { FileUploader } from "./FileUploader.jsx";
 
-    const [selectedFile,setSelectedFile] = useState('');
-    const [selectedExp,setSelectedExp] = useState('');
-    const [response,setResponse] = useState('');
+const Dashboard = () => {
+    const [selectedFile, setSelectedFile] = useState('');
+    const [selectedExp, setSelectedExp] = useState('');
+    const [response, setResponse] = useState('');
     const [responseRecieved, setResponseRecieved] = useState(false);
-    const [data,setdata] = useState([]);
-    const userData = sessionStorage.getItem('userData')
+    const [data, setdata] = useState([]);
+    const [dragging, setDragging] = useState(false); // State to track dragging state
+    const [isDroppingFiles, setIsDroppingFiles] = useState(false); // State to track file dropping
+    const userData = sessionStorage.getItem('userData');
     const parsedData = JSON.parse(userData);
     const selectedBatch = parsedData.selectedBatch;
     const selectedRollNo = parsedData.selectedRollNo;
     const selectedLab = parsedData.selectedLab;
-    useEffect(()=>{
-        // Parse JSON string to JavaScript object
+    const [selectedExpName, setSelectedExpName] = useState(''); // State to store selectedExpName 
+    const [filebool,setfilebool] = useState(false);
+    
+    useEffect(() => {
+        fetch(`http://127.0.0.1:8000/api/exp?batch=${selectedBatch}&roll_no=${selectedRollNo}&lab_id=${selectedLab}`, {
+            method: 'GET',
+        })
+        .then(res => res.json())
+        .then(data => {
+            const expOptions = Object.entries(data).map(([expId, expName]) => ({
+                id: expId,
+                name: expName
+            }));
+            setdata(expOptions);
+        })
+        .then(() => setResponseRecieved(true))
+        .catch(error => console.log(error));
+    }, []);
 
-            // console.log("Use effect has been initiated the values of the selectedBatch, selectedRollNo and selectedLab are ",selectedBatch,selectedRollNo,selectedLab)
-            fetch(`http://127.0.0.1:8000/api/exp?batch=${selectedBatch}&roll_no=${selectedRollNo}&lab_id=${selectedLab}`, {
-                method: 'GET',
-            })
-            .then(res => res.json())
-            .then(data => {
-                const expOptions = Object.entries(data).map(([expId, expName]) => ({
-                    id: expId,
-                    name: expName
-                }));
-                setdata(expOptions);
-            })
-            
-            .then(() => 
-            setResponseRecieved(true)
-            )
-            .catch(error => console.log(error))
-        
-    },[])
-    function handleSubmit(e){
-        // e.preventDefault();
+
+    function handleSubmit() {
         const formData = new FormData();
         formData.append('batch', selectedBatch);
         formData.append('roll_no', selectedRollNo);
         formData.append('lab_id', selectedLab);
         formData.append('exp_id', selectedExp);
-        formData.append('file',selectedFile);
-        fetch('http://127.0.0.1:8000/api/file',{
+        formData.append('file', selectedFile);
+        // console.log(selectedFile)
+        // console.log(selectedExp)
+        fetch('http://127.0.0.1:8000/api/file', {
             method: 'POST',
-            body : formData
-        }).then(res => setResponse(res.statusText))
-        .then(console.log(response))
-        .catch(error => console.log(error))
-        console.log(response)
-        // window.location.reload()
+            body: formData
+        })
+        .then(res => setResponse(res.statusText)).then(window.location.reload())
+        .catch(error => console.log(error));
+        
+    }
 
-    }
-    
-    function handleUpload(e){
-        setSelectedFile(e.target.files[0])
-    }
+    function DragDrop({ exp }) {
+        const [fileName, setFileName] = useState("");
+        const handleFile =   (file) => {
+            setSelectedFile(file);
+            setFileName(file.name);
+            setSelectedExp(exp.id);
+            setSelectedExpName(exp.name.exp_name);
+            setfilebool(true);
+        };
+      return (
+          <div className="file-uploader">
+               <FileUploader handleFile={handleFile}  />
+              
+              
+          </div>
+      );
+  }
 
     return (
         <>
-        <div>
-            <h1>Student Dashboard</h1>
-            <table>
-            <thead>
-                <tr>
-                <th>Experiment Name</th>
-                <th>Uploaded</th>
-                <th>Printed</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map(exp => (
-                <tr key={exp.id}>
-                    <td>{exp.name.exp_name}</td>
-
-                    <td>{exp.name.uploaded ? <a href={"http://127.0.0.1:8000/" + exp.name.url}>Uploaded</a> : 
-                    <form >
-                        <input type="file" onChange={(e)=>{setSelectedExp(exp.id);handleUpload(e)}} />
-                        <button type="submit" onClick={handleSubmit}>Upload</button>
-                    </form>}
-                    </td>
-
-                    <td>{exp.name.printed ? 'Printed' : 'Not Printed'}</td>
-                </tr>
-                ))}
-            </tbody>
-            </table>
-        </div>
-    </>
-        
-    )
+        {filebool &&
+         <div className='confirm'>
+            <h2>Are you sure you want to upload {selectedFile.name} as {selectedExpName}?</h2>
+            <button className='yes' onClick={handleSubmit}>Yes</button>
+            <button className='no' onClick={() => setfilebool(false)}>No</button>    
+        </div>}
+            <div className="container-dashboard">
+                <div className="nav">
+                    <h1 className='headertext'>Dash<span>board</span></h1>
+                </div>
+                
+                <table>
+                    <thead className='table_head'>
+                        <tr>
+                            <th className='head'>Experiment</th>
+                            <th className='head'>Upload status</th>
+                            <th className='head'>Print Status</th>
+                        </tr>
+                    </thead>
+                    <tbody className='table_body'>
+                        {data.map(exp => (
+                            <tr key={exp.id}>
+                                <td>{exp.name.exp_name}</td>
+                                <td>
+                                    {exp.name.uploaded ? (
+                                        <a href={"http://127.0.0.1:8000/" + exp.name.url}>Uploaded</a>
+                                    ) : (
+                                        <div>
+                                            <DragDrop exp={exp} />
+                                            
+                                        </div>
+                                    )}
+                                </td>
+                                <td>{exp.name.printed ? 'Printed' : 'Not Printed'}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <Link to="/student/login">Logout</Link>
+            </div>
+        </>
+    );
 }
 
-export default Dashboard
+export default Dashboard;
---
+++ ProjectForm/src/Components/FileUploader.jsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import "../assets/dashboard.css";
+export const FileUploader = ({ handleFile }) => {
+  // Create a reference to the hidden file input element
+  const hiddenFileInput = useRef(null);
+
+  // Programatically click the hidden file input element
+  // when the Button component is clicked
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+  // Call a function (passed as a prop from the parent component)
+  // to handle the user-selected file
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    handleFile(fileUploaded);
+  };
+  return (
+    <>
+      <button className="button-upload" onClick={handleClick}>
+        Upload a file
+      </button>
+      <input
+        type="file"
+        onChange={handleChange}
+        ref={hiddenFileInput}
+        style={{ display: "none" }} // Make the file input element invisible
+      />
+    </>
+  );
+};
